b388192c66b1071da3b466bfe99825d0ee4426d1
Takumi Ogawa <ogataku01123@gmail.com>
2019-02-23 01:31:05 +0900

    init

---
+++ microblog/microblog/settings/heroku.py
@@ -0,0 +1,15 @@
+from .common import *
+import dj_database_url
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = { 'HTTP_X_FORWAREDED_PRONTO', 'http'}
+
+ALLOWED_HOSTS = ["*"]
+
---
+++ microblog/microblog/wsgi.py
@@ -0,0 +1,38 @@
+"""
+WSGI config for microblog project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+import threading
+import requests
+import time
+
+from django.core.wsgi import get_wsgi_application
+
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'microblog.settings.heroku')
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
+
+# スリープ防止：デプロイしたWebサーバー自体が自分にアクセス
+def awake():
+    while True:
+        try:
+            print("Start Awaking")
+            requests.get("https://anicolle.herokuapp.com/")
+            print("End")
+        except:
+            print("error")
+        time.sleep(300)
+
+
+t = threading.Thread(target=awake)
+t.start()
