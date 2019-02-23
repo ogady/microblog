b6c0f8c8fa80d671bd471180cf7cbb0dbe79f8c3
Takumi Ogawa <ogataku01123@gmail.com>
2019-02-23 14:55:16 +0900

    軽微な修正

--- microblog/microblog/settings/heroku.py
+++ microblog/microblog/settings/heroku.py
@@ -7,22 +7,7 @@ DEBUG = False
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        # sqlite
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
-        # MySQL
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'anicolle',
-        # 'USER': 'root',
-        # 'PASSWORD': 'password',
-        # 'HOST': '*',  # (ローカルホストなら空でも可)
-        # 'PORT': '*',  # (デフォルトポートなら空でも可)
-
-    }
-}
+DATABASES = {'default'}
 
 SECURE_PROXY_SSL_HEADER = {'HTTP_X_FORWARDED_PRONTO', 'http'}
 
