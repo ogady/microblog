79cd5c88dc6924c1a4942294e97322323d0a33ec
Takumi Ogawa <ogataku01123@gmail.com>
2019-02-23 14:07:16 +0900

    databaseをmysqlへと変更

--- microblog/microblog/settings/heroku.py
+++ microblog/microblog/settings/heroku.py
@@ -7,9 +7,24 @@ DEBUG = False
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': {
+        # sqlite
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
-SECURE_PROXY_SSL_HEADER = { 'HTTP_X_FORWAREDED_PRONTO', 'http'}
+        # MySQL
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'anicolle',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '*',  # (ローカルホストなら空でも可)
+        'PORT': '*',  # (デフォルトポートなら空でも可)
+
+    }
+}
+
+SECURE_PROXY_SSL_HEADER = { 'HTTP_X_FORWARDED_PRONTO', 'http'}
 
 ALLOWED_HOSTS = ["*"]
 
