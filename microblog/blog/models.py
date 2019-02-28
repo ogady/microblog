e63e6a70a22630d5a53b4fc2a31990f67e723371
Takumi Ogawa <ogataku01123@gmail.com>
2019-02-28 23:23:26 +0900

    #15

--- microblog/blog/models.py
+++ microblog/blog/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Blog(models.Model):
     content = models.CharField(max_length=140)
     photo = models.ImageField(upload_to='anicolleblog', blank=True, null=True)
-    anime = models.IntegerField(blank=True, null=True)
+    anime_id = models.IntegerField(blank=True, null=True)
     anime = models.CharField(max_length=200, blank=True, null=True)
     posted_date = models.DateTimeField(auto_now_add=True)
 
