308a2bdd4a450a1782bb5e50408682664c943a0c
Takumi Ogawa <ogataku01123@gmail.com>
2019-03-02 21:39:10 +0900

    #4

--- microblog/blog/forms.py
+++ microblog/blog/forms.py
@@ -1,11 +1,13 @@
 from django import forms
-from .models import Blog
+from .models import Blog, User
 
 
 class BlogForm(forms.ModelForm):
+
+    user = User()
     content = forms.CharField(widget=forms.TextInput(attrs={"size": 60}))
     photo = forms.ImageField(required=False)
 
     class Meta:
         model = Blog
-        fields = ["content", "photo"]
+        fields = ["user", "content", "photo"]
--- microblog/microblog/settings.py
+++ microblog/microblog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'bootstrap4',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
