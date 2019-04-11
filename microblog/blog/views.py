1f3b8e3143cba8b994bd0223bc8cb134fc0fffde
Takumi Ogawa <ogataku01123@gmail.com>
2019-04-11 23:18:20 +0900

    [update]ダグ機能を実装

--- microblog/blog/views.py
+++ microblog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView, View
 from django.contrib import messages
@@ -7,9 +7,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from django.http.response import JsonResponse
-from .models import Blog, Comment, Like, UserProfile
-from .forms import BlogForm, SearchForm, UserCreateForm, LoginForm, CommentForm,  UserUpdateForm, ProfileFormSet
+from .models import Blog, Comment, Like, UserProfile, Tag
+from .forms import BlogForm, SearchForm, UserCreateForm, LoginForm, \
+    CommentForm, UserUpdateForm, ProfileFormSet, TagInlineFormSet
 import requests
+import re
 
 User = get_user_model()
 
@@ -37,6 +39,13 @@ class BlogDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         # 記事へのコメントを取得
         context['comment_list'] = self.object.comment_set.filter(parent__isnull=True)
+        tags = self.object.tag.filter(blog=kwargs['object'])
+        tag_list = []
+
+        for tag in tags:
+            tag_list.append(str(tag.name))
+
+        context['tags'] = tag_list
         context['like_cnt'] = Like.objects.filter(post=kwargs['object']).count()
         return context
 
@@ -52,12 +61,36 @@ class BlogCreateView(LoginRequiredMixin, CreateView):
     # templateをクラス汎用ビューのデフォルトから変える
     template_name = "blog/blog_create_form.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(BlogCreateView, self).get_context_data(**kwargs)
+        context.update(dict(formset=TagInlineFormSet(self.request.POST or None, instance=self.object)))
+
+        return context
+
     # バリデート後
     def form_valid(self, form):
-        # self.requestオブジェクトに”保存しました”を込める
-        messages.success(self.request, "保存しました。")
-        # super()で継承元の処理を返すのがお約束
-        return super().form_valid(form)
+
+        context = self.get_context_data()
+        formset = context['formset']
+        if formset.is_valid():
+            tags = self.request.POST.get("tag")
+            tag_list = re.split("[,、]", tags)
+
+            blog = form.save(commit=False)
+            blog.save()
+
+            for tag in tag_list:
+                tag_obj = Tag(name=tag)
+                tag_obj.save()
+                blog.tag.add(tag_obj)
+
+            messages.success(self.request, "保存しました。")
+            return super().form_valid(form)
+
+        else:
+            context['form'] = form
+            messages.error(self.request, "保存に失敗しました。")
+            return self.render_to_response(context)
 
     def form_invalid(self, form):
         # self.requestオブジェクトに”保存に失敗しました。”を込める
@@ -69,8 +102,19 @@ class BlogUpdateView(LoginRequiredMixin, UpdateView):
     model = Blog
     form_class = BlogForm
 
-    # LoginRequiredMixinを使う際は定義する必要あり
     login_url = '/login'
+
+    def get_context_data(self, **kwargs):
+        context = super(BlogUpdateView, self).get_context_data(**kwargs)
+        context.update(dict(formset=TagInlineFormSet(self.request.POST or None, instance=self.object)))
+        tags = self.object.tag.filter(**kwargs)
+        tag_list = []
+
+        for tag in tags:
+            tag_list.append(str(tag.name))
+
+        context['tags'] = tag_list
+        return context
 
     # success_urlを自作する
     def get_success_url(self):
@@ -86,9 +130,25 @@ class BlogUpdateView(LoginRequiredMixin, UpdateView):
 
     # バリデート後
     def form_valid(self, form):
-        # self.requestオブジェクトに”更新しました。”を込める
+
+        tags = self.request.POST.get("tag")
+        tag_list = re.split("[,、]", tags)
+
+        blog = form.save(commit=False)
+        blog.save()
+
+        if not tags:
+            messages.success(self.request, "更新しました。")
+            return super().form_valid(form)
+
+        blog.tag.clear()
+
+        for tag in tag_list:
+            tag_obj = Tag(name=tag)
+            tag_obj.save()
+            blog.tag.add(tag_obj)
+
         messages.success(self.request, "更新しました。")
-        # super()で継承元の処理を返すのがお約束
         return super().form_valid(form)
 
     def form_invalid(self, form):
@@ -375,12 +435,12 @@ class LikeAddOrDeleteApi(LoginRequiredMixin, View):
 
         # いいねを解除する
         if is_like > 0:
-           liking = Like.objects.get(post__id=kwargs['blog_pk'], user=request.user)
-           liking.delete()
-           blog.like_num -= 1
-           blog.save()
+            liking = Like.objects.get(post__id=kwargs['blog_pk'], user=request.user)
+            liking.delete()
+            blog.like_num -= 1
+            blog.save()
 
-           return JsonResponse({"like": blog.like_num})
+            return JsonResponse({"like": blog.like_num})
 
         # いいねする
         blog.like_num += 1
