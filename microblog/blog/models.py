7102449a2d446f06cc72c966208b71cef75e4eac
Takumi Ogawa <ogataku01123@gmail.com>
2019-04-15 01:03:25 +0900

    [fix]models修正

--- microblog/blog/models.py
+++ microblog/blog/models.py
@@ -14,7 +14,7 @@ from .exeption_models import BaseManager
 class Tag(models.Model):
     """ タグ """
     objects = BaseManager()
-    name = models.CharField(max_length=255, null=False)
+    name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
